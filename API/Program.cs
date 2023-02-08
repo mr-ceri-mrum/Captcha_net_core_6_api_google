5ef8e6c8acbf0b49385a4248ec071f58ef39af70
makabake9 <zhunussoffilias0216@gmail.com>
2023-02-08 18:52:38 +0600

    first commit2

---
+++ API/Program.cs
@@ -0,0 +1,29 @@
+using API;
+using API.Interfaces;
+using API.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection(nameof(AppSettings)));
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<ISignupService, SignupService>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
